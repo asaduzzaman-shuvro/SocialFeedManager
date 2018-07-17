a4ba082569b363d38789f3c462d1a24dcb21e9e1
atiqul alam <atiqul@smartitengineering.com>
2018-07-17 23:29:33 +0600

    Add thirdparty api folder where all feed will return. Create for facebook dummy feed proccessor.

---
+++ src/main/java/com/thirdparty/api/FacebookApi.java
@@ -0,0 +1,14 @@
+package com.thirdparty.api;
+
+/**
+ * Created by atiqul on 7/17/2018.
+ */
+public class FacebookApi {
+
+  public String processFeed() {
+
+    return "facebook feeds";
+
+  }
+
+}
