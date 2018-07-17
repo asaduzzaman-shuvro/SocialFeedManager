49ebdb4f271856be6858ffe9f3518d106c46168c
atiqul alam <atiqul@smartitengineering.com>
2018-07-17 22:31:10 +0600

    Initiate project with a dummy main class and .gitignore file.

---
+++ src/main/java/com/cefalo/school/Main.java
@@ -0,0 +1,11 @@
+package com.cefalo.school;
+
+/**
+ * Created by atiqul on 7/17/2018.
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Main class");
+  }
+}
