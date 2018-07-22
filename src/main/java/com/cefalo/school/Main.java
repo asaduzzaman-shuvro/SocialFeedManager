4510a6810cca8ae9b3b887ddd9bd88d8bbbbb27a
atiqul alam <atiqul@smartitengineering.com>
2018-07-22 22:54:48 +0600

    Create 3 new method to test our implementation for 3 social feeds in Main class to avoid further conflicts.

--- src/main/java/com/cefalo/school/Main.java
+++ src/main/java/com/cefalo/school/Main.java
@@ -1,8 +1,12 @@
 package com.cefalo.school;
 
+import com.cefalo.school.model.FeedItem;
+import com.cefalo.school.operators.FacebookOperator;
+import com.cefalo.school.processors.FacebookFeedProcessor;
 import com.thirdparty.api.FacebookApi;
-import com.thirdparty.api.InstagramApi;
-import com.thirdparty.api.TwitterApi;
+import java.util.List;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  * Created by atiqul on 7/17/2018.
@@ -10,6 +14,26 @@ import com.thirdparty.api.TwitterApi;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println(TwitterApi.getFeeds());
+
+    facebookTest();
+
+  }
+
+  public static void facebookTest(){
+    FacebookFeedProcessor facebookFeedProcessor = new FacebookFeedProcessor();
+    List<FeedItem> feedItems = facebookFeedProcessor.getFeedItems();
+
+    for(FeedItem item : feedItems){
+      String message = item.contents.get(0).value;
+      System.out.println(message);
+    }
+  }
+
+  public static void twitterTest(){
+
+  }
+
+  public static void instagramTest(){
+
   }
 }
