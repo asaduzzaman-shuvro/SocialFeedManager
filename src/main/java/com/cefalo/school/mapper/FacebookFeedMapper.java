05159447a17047a5352ed427789c529751c4a34f
atiqul alam <atiqul@smartitengineering.com>
2018-07-22 22:51:16 +0600

    Refactor FacebookFeedMapper class to resolve exceptions.

--- src/main/java/com/cefalo/school/mapper/FacebookFeedMapper.java
+++ src/main/java/com/cefalo/school/mapper/FacebookFeedMapper.java
@@ -15,35 +15,35 @@ import java.util.Locale;
 public class FacebookFeedMapper implements FeedMapper {
     public List<FeedItem> getProcessedFeedItems(JSONObject jsonObject) {
 
-        JSONArray array = jsonObject.getJSONArray("data");
+        JSONArray data = jsonObject.getJSONArray("data");
 
         List<FeedItem> feedItemList = new ArrayList<FeedItem>();
 
-        for (int i = 0; i < array.length(); i++) {
+        for (Object item : data) {
 
-            JSONObject object = array.getJSONObject(i);
+            JSONObject object = (JSONObject) item;
 
             FeedItem feedItem = new FeedItem();
             feedItem.identifier = object.getString("id");
             try {
-                feedItem.publishedDate = new SimpleDateFormat("yyyy-MM-ddThh:mm:ssZ", Locale.ENGLISH).parse(object.getString("created_time")) ;
+                feedItem.publishedDate = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ssZ", Locale.ENGLISH).parse(object.getString("created_time")) ;
             }catch (Exception e) {
-                System.out.println(e);
+                e.printStackTrace();
             }
 
-            if (object.getString("message") != null) {
+            if (object.has("message")) {
                 feedItem.contents.add(new Content(ContentType.TEXT, object.getString("message")));
             }
 
-            if (object.getString("links") != null) {
+            if (object.has("links")) {
                 feedItem.contents.add(new Content(ContentType.URL, object.getString("links")));
             }
 
-            if (object.getString("video") != null) {
+            if (object.has("video")) {
                 feedItem.contents.add(new Content(ContentType.VIDEO, object.getString("video")));
             }
 
-            if (object.getString("image") != null) {
+            if (object.has("image")) {
                 feedItem.contents.add(new Content(ContentType.PICTURE, object.getString("image")));
             }
 
