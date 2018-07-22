250bcf0f877333e9c01348758278382d34faeccb
Asaduzzaman Shuvro <asaduzzaman.duet@gmail.com>
2018-07-22 14:13:12 +0600

    Twitter mapper functionality added.

--- src/main/java/com/cefalo/school/mapper/TwitterFeedMapper.java
+++ src/main/java/com/cefalo/school/mapper/TwitterFeedMapper.java
@@ -1,11 +1,44 @@
 package com.cefalo.school.mapper;
 
+import com.cefalo.school.model.Content;
+import com.cefalo.school.model.ContentType;
 import com.cefalo.school.model.FeedItem;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class TwitterFeedMapper implements FeedMapper {
     public List<FeedItem> getProcessedFeedItems() {
-        return null;
+
+        JSONObject jsonObject = null;
+        JSONArray array = jsonObject.getJSONArray("data");
+
+        List<FeedItem> feedItemList = null;
+
+        for (int i = 0; i < array.length(); i++) {
+
+            JSONObject object = array.getJSONObject(i);
+
+            FeedItem feedItem = new FeedItem();
+            feedItem.identifier = object.getString("id_str");
+
+            try {
+                feedItem.publishedDate = new SimpleDateFormat("EE MMM dd hh:mm:ss Z yyyy", Locale.ENGLISH).parse(object.getString("created_at")) ;
+            }catch (Exception e) {
+                System.out.println(e);
+            }
+
+            if (object.getString("text") != null) {
+                feedItem.contents.add(new Content(ContentType.TEXT, object.getString("text")));
+            }
+
+            feedItemList.add(feedItem);
+        }
+
+        return feedItemList;
     }
 }
