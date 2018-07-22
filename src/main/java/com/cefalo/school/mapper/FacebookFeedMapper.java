631d908e29b67bdc54779ad9646a909fd2a817b3
Asaduzzaman Shuvro <asaduzzaman.duet@gmail.com>
2018-07-22 11:07:30 +0600

    map the FacebookFeedMapper

--- src/main/java/com/cefalo/school/mapper/FacebookFeedMapper.java
+++ src/main/java/com/cefalo/school/mapper/FacebookFeedMapper.java
@@ -1,12 +1,55 @@
 package com.cefalo.school.mapper;
 
+import com.cefalo.school.model.Content;
+import com.cefalo.school.model.ContentType;
 import com.cefalo.school.model.FeedItem;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class FacebookFeedMapper implements FeedMapper {
     public List<FeedItem> getProcessedFeedItems() {
 
-        return null;
+        JSONObject jsonObject = null;
+        JSONArray array = jsonObject.getJSONArray("data");
+
+        List<FeedItem> feedItemList = null;
+
+        for (int i = 0; i < array.length(); i++) {
+
+            JSONObject object = array.getJSONObject(i);
+
+            FeedItem feedItem = new FeedItem();
+            feedItem.identifier = object.getString("id");
+            try {
+                feedItem.publishedDate = new SimpleDateFormat("yyyy-MM-ddThh:mm:ssZ", Locale.ENGLISH).parse(object.getString("created_time")) ;
+            }catch (Exception e) {
+                System.out.println(e);
+            }
+
+            if (object.getString("message") != null) {
+                feedItem.contents.add(new Content(ContentType.TEXT, object.getString("message")));
+            }
+
+            if (object.getString("links") != null) {
+                feedItem.contents.add(new Content(ContentType.URL, object.getString("links")));
+            }
+
+            if (object.getString("video") != null) {
+                feedItem.contents.add(new Content(ContentType.VIDEO, object.getString("video")));
+            }
+
+            if (object.getString("image") != null) {
+                feedItem.contents.add(new Content(ContentType.PICTURE, object.getString("image")));
+            }
+
+            feedItemList.add(feedItem);
+        }
+
+        return feedItemList;
     }
 }
--- src/main/java/com/cefalo/school/model/Content.java
+++ src/main/java/com/cefalo/school/model/Content.java
@@ -3,4 +3,9 @@ package com.cefalo.school.model;
 public class Content {
     public ContentType contentType;
     public String value; // value could be text, image url, external url or video url
+
+    public Content(ContentType contentType, String value) {
+        this.contentType = contentType;
+        this.value = value;
+    }
 }
